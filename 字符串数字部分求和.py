c924e1ca47fbfda92900243029e63a5a00320089
rywbl <37326700+rywbl@users.noreply.github.com>
2019-05-26 14:19:18 +0800

    Add files via upload

---
+++ 字符串数字部分求和.py
@@ -0,0 +1,11 @@
+﻿#字符串中数字部分求和
+print("字符串中数字部分求和")
+str = input("请输入一个混合字符串")
+index = 0
+sum = 0
+while index < len(str):
+	if str[index] >= "0" and str[index] <= "9":
+		sum += int(str[index])
+	index += 1
+print("sum = %d" %(sum))
+
