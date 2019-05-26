00d6f049ae58963b059bba560b2d1f464ef9196d
rywbl <37326700+rywbl@users.noreply.github.com>
2019-05-26 15:47:43 +0800

    Add files via upload

---
+++ 五人平均年龄 列表代码优化.py
@@ -0,0 +1,17 @@
+﻿#存储5个人的年龄，求他们的平均年龄
+age1 = 18
+age2 = 19
+age3 = 20
+age4 = 21
+age5 = 22
+print((age1 + age2 + age3 + age4 + age5) / 5)
+
+#列表代码优化
+list = [18,19,20,21,22]
+index = 0
+sum = 0
+while index < 5:
+	sum += list[index]
+	index += 1
+	if index ==5 :
+		print("平均年龄：%d" %(sum / 5))
